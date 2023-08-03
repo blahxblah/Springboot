eea065c9df4d54312732121b24c5570982cfe3c1
blahxblah <id.blahxblah@gmail.com>
2023-08-03 16:43:31 +0900

    init

---
+++ src/main/java/com/Gitflow/Springboot/kjh/ResumeRepository.java
@@ -0,0 +1,7 @@
+package com.Gitflow.Springboot.kjh;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumeRepository extends JpaRepository<Resume, Integer>{
+
+}
